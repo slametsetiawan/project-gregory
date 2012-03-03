029e1bbca85eb46e7f934b65e38277961ac1d293
lee.lung.ming@gmail.com <lee.lung.ming@gmail.com@8e6abcae-c01e-542e-93b6-ba7f58710efa>
2012-03-03 04:06:22 +0000

    menghapus widget2 ngga guna... menambah widget galeri_produk mengubah widget formulir_login

--- trunk/widget/galeri_produk.php
+++ trunk/widget/galeri_produk.php
@@ -2,9 +2,70 @@
 
 function widget_galeri_produk()
 {
+    $sumber_data_produk_acak = mysql_query("
+        SELECT
+            *
+        FROM
+            produk
+        ORDER BY RAND()
+        LIMIT 0, 8");
     ?>
     <div class="widget_galeri_produk">
-        
+        <table>
+            <?php while($produk = mysql_fetch_assoc($sumber_data_produk_acak)):?>
+                <tr>
+                    <td>
+                        <a href="<?php echo(buat_url("produk", array("no"=>$produk["no"])));?>">
+                            <img
+                                src="<?php echo(url_dasar());?>/images/produk/<?php echo($produk["no"]);?>/<?php echo(rand(1,4));?>.jpg"
+                                width="60"
+                                height="80"
+                                alt="<?php echo($produk["kode"])?> | <?php echo($produk["nama"]);?>"
+                            />
+                        </a>
+                    </td>
+                    <td>
+                        <?php $produk = mysql_fetch_assoc($sumber_data_produk_acak);?>
+                        <?php if(!empty($produk["no"])):?>
+                            <a href="<?php echo(buat_url("produk", array("no"=>$produk["no"])));?>">
+                                <img
+                                    src="<?php echo(url_dasar());?>/images/produk/<?php echo($produk["no"]);?>/<?php echo(rand(1,4));?>.jpg"
+                                    width="60"
+                                    height="80"
+                                    alt="<?php echo($produk["kode"])?> | <?php echo($produk["nama"]);?>"
+                                />
+                            </a>
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <?php $produk = mysql_fetch_assoc($sumber_data_produk_acak);?>
+                        <?php if(!empty($produk["no"])):?>
+                            <a href="<?php echo(buat_url("produk", array("no"=>$produk["no"])));?>">
+                                <img
+                                    src="<?php echo(url_dasar());?>/images/produk/<?php echo($produk["no"]);?>/<?php echo(rand(1,4));?>.jpg"
+                                    width="60"
+                                    height="80"
+                                    alt="<?php echo($produk["kode"])?> | <?php echo($produk["nama"]);?>"
+                                />
+                            </a>
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <?php $produk = mysql_fetch_assoc($sumber_data_produk_acak);?>
+                        <?php if(!empty($produk["no"])):?>
+                            <a href="<?php echo(buat_url("produk", array("no"=>$produk["no"])));?>">
+                                <img
+                                    src="<?php echo(url_dasar());?>/images/produk/<?php echo($produk["no"]);?>/<?php echo(rand(1,4));?>.jpg"
+                                    width="60"
+                                    height="80"
+                                    alt="<?php echo($produk["kode"])?> | <?php echo($produk["nama"]);?>"
+                                />
+                            </a>
+                        <?php endif; ?>
+                    </td>
+                </tr>
+            <?php endwhile;?>
+        </table>
     </div>
     <?php
 }

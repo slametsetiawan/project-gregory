001969d4774be4dead3dbc56c90b8e47f8982f32
lee.lung.ming <lee.lung.ming@gmail.com>
2012-03-03 13:41:21 +0000

    Becanda'e mari deh...  ha ha ha...

--- konfigurasi.php
+++ konfigurasi.php
@@ -5,27 +5,49 @@ $GLOBALS["dbuser"] = "root";
 $GLOBALS["dbpass"] = "";
 $GLOBALS["dbname"] = "perdagangan_elektronik";
 
-$GLOBALS["nama_situs"] = "TokoMaho.Co.Li";
-$GLOBALS["slogan_situs"] = "Ini situs Maho Gan... =))";
+$GLOBALS["nama_situs"] = "BerBaju.Com";
+$GLOBALS["slogan_situs"] = "Toko Baju Online yang bersahabat";
 $GLOBALS["deskripsi_situs"] = "
-    <p style=\"text-align:justify;\">
-        Mau punya RUMAH MEWAH, MOBIL, dan HIDUP MEWAH dengan TANPA MODAL dan TANPA UTANG, anda berada di situs yang benar.
-        Follow Us dengan acara GOLDEN WAYS bersama GregoryTetapTeguhSelalu.
-        Kalau dengan dengan cara umumnya ibarat ANDA MENGEJAR UANG,
-        dengan GOLDENWAYS bersama GregoryTetapTeguhSelalu, UANG YANG MENGEJAR ANDA.
-    </p>
-    TTD.</br>
-    MaHo's Generations. =))";
+    <p style=\"text-align:justify; text-indent:50px;\">
+        BerBaju.Com merupakan toko baju online, toko baju online terlengkap di Indonesia.
+        Toko baju grosir online,
+        toko baju online menyediakan baju impor,
+        pakaian batik,
+        baju muslim,
+        baju batik,
+        baju wanita,
+        baju dalaman,
+        baju tidur,
+        pakaian pesta,
+        busana muslim,
+        pakaian muslim,
+        baju anak,
+        baju pria,
+        baju cowok,
+        baju cewek,
+        baju lingerie,
+        kacamata,
+        tas impor,
+        mainan anak,
+        alat tulis,
+        aksesoris mobil,
+        tas anak,
+        sandal dan sebagainya.
+        Melayani pembelian baju,
+        pakaian grosir atau eceran.
+        Harga sangat terjangkau,
+        dan koleksinya sangat lengkap.
+    </p>";
 $GLOBALS["tentang_situs"] = "
-    <p>
-        It's time for MaHo's Generations. kwkwkwkwkwkwkwk.....
-    </p>
-    TTD.<br/>
-    MaHo's Generations. =))";
+    <p style=\"text-align:justify; text-indent:50px;\">
+        Kami mempunyai beberapa butik dan toko di kota Surabaya, yang berdiri sejak 1998.
+        Di tahun 2007 kami membuat toko online pertama kami, yaitu Bursabaju.com.
+        Dengan adanya dukungan dari para customer kami, baik grosiran dan retail, kami bisa terus berkembang.
+        Di tahun 2010 ini kami meluncurkan Bursabaju.com versi kedua, dengan desain baru yang lebih eksotis, elegan dan menawan.
+    </p>";
 
 $GLOBALS["ym_cs"] = array(
-    "glayzher&m=g" => "GregoryTetapTeguhSelalu",
-    "glayzher" => "Maho's Generations's Leader.",
+    "glayzher" => "Gregory Adrianto",
 );
 
 ?>
